793e177e352d791039f8d080bca6e68c4e1aa9c0
robbymcd <99295997+robbymcd@users.noreply.github.com>
2025-03-28 15:48:45 -0400

    setup shadcn and zod + built login page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,88 @@
-import Image from "next/image";
+"use client"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Toaster } from "../components/ui/sonner";
+import { toast } from "sonner";
+import { Button } from "../components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../components/ui/form";
+import { Input } from "../components/ui/input";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
 import styles from "./page.module.css";
 
+const formSchema = z.object({
+  email: z.string().email().endsWith(".edu", {
+    message: "Email must be a valid school email address"
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long"
+  }),
+})
+
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    toast.success("Login successful");
+    form.reset();
+    // 
+  }
+
   return (
-    <div>
-      
+    <div className={styles.page}>
+      <Card className={styles.card}>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className={styles.form}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input className={styles.input} placeholder="Enter your email here" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input className={styles.input} type="password" placeholder="Enter your password here" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className={styles.button} type="submit">Submit</Button>
+            </form>
+          </Form>
+          <div className={styles.footer}>
+            <p>Don't have an account?</p>
+            <a href="/signup">Sign up</a>
+          </div>
+        </CardContent>
+      </Card>
+      <Toaster />
     </div>
   );
 }
