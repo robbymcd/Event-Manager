12230ec2e4cdc5973e4773b855dfdfe68948e667
matheus-seabra <100260792+matheus-seabra@users.noreply.github.com>
2025-04-08 15:30:21 -0400

    Fixing SQL again (my bad)

--- tables.sql
+++ tables.sql
@@ -1,57 +1,51 @@
 CREATE TABLE university (
-     id SERIAL PRIMARY KEY,
-     name VARCHAR(255) NOT NULL,
-     location VARCHAR(255),
-     numstudent INT,
-     description TEXT
- );
- 
- CREATE TABLE users (
-     id SERIAL PRIMARY KEY,
-     email VARCHAR(255) UNIQUE NOT NULL,
-     password VARCHAR(255) NOT NULL,
-     role VARCHAR(50) NOT NULL,
-     university INT,
-     rso TEXT[],
- 
-     FOREIGN KEY (university) REFERENCES university(id)
- );
- 
- CREATE TABLE rso (
-     id SERIAL PRIMARY KEY,
-     name VARCHAR(255) NOT NULL,
-     university VARCHAR(255),
-     description TEXT,
-     category VARCHAR(100)
- );
- 
- CREATE TABLE events (
-     id SERIAL PRIMARY KEY,
-     name VARCHAR(255) NOT NULL,
- @@ -27,6 +34,10 @@
-     contact_phone VARCHAR(20),
-     contact_email VARCHAR(255),
-     participants TEXT[]
-     rso INT NOT NULL,
-     university INT NOT NULL,
-     FOREIGN KEY (rso) REFERENCES rso(id),
-     FOREIGN KEY (university) REFERENCES university(id)
- );
- 
- CREATE TABLE ratings (
- @@ -35,15 +46,6 @@
-     event_id INT NOT NULL,
-     rating INT CHECK (rating BETWEEN 1 AND 5),
-     comments TEXT[],
- 
-     FOREIGN KEY (user_id) REFERENCES users(id),
-     FOREIGN KEY (event_id) REFERENCES events(id)
- );
- 
- CREATE TABLE rso (
-     id SERIAL PRIMARY KEY,
-     name VARCHAR(255) NOT NULL,
-     university VARCHAR(255),
-     description TEXT,
-     category VARCHAR(100)
- );
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255),
+    numstudent INT,
+    description TEXT
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    university INT,
+    rso TEXT[],
+    FOREIGN KEY (university) REFERENCES university(id)
+);
+
+CREATE TABLE rso (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    university VARCHAR(255),
+    description TEXT,
+    category VARCHAR(100)
+);
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    description TEXT,
+    event_time TIMESTAMP NOT NULL,
+    location VARCHAR(255),
+    contact_phone VARCHAR(20),
+    contact_email VARCHAR(255),
+    participants TEXT[],
+    rso INT NOT NULL,
+    university INT NOT NULL,
+    FOREIGN KEY (rso) REFERENCES rso(id),
+    FOREIGN KEY (university) REFERENCES university(id)
+);
+
+CREATE TABLE ratings (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comments TEXT[],
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
